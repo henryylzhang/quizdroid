95121a4553952a5bb771ab27d486e12ed981fcdf
Henry Zhang <henry.zhang@live.com>
2018-04-22 00:20:43 -0700

    Initial commit

---
+++ app/src/main/java/edu/washington/zhang007/quizdroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.washington.zhang007.quizdroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
