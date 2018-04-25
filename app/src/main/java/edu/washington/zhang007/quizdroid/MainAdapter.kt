4f69913b3652b5b6a3b5c412e4d4a895ea61f4db
Henry Zhang <henry.zhang@live.com>
2018-04-25 02:50:13 -0700

    Add RecyclerView with topics

---
+++ app/src/main/java/edu/washington/zhang007/quizdroid/MainAdapter.kt
@@ -0,0 +1,29 @@
+package edu.washington.zhang007.quizdroid
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.topic_layout.view.*
+
+class MainAdapter : RecyclerView.Adapter<ViewHolder>() {
+
+    private val topicTitles = listOf("Math", "Physics", "Marvel Superheroes")
+
+    override fun getItemCount(): Int {
+        return topicTitles.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val topic = layoutInflater.inflate(R.layout.topic_layout, parent, false)
+        return ViewHolder(topic)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val topicTitle = topicTitles[position]
+        holder.view.textView_topic.text = topicTitle
+    }
+}
+
+class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
